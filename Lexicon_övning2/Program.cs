befc8eb60999e3c8efc7aa541f8f47bc278334ce
Heithem980 <heithem.gha98@gmail.com>
2024-05-21 15:06:23 +0200

    Add project files.

---
+++ Lexicon_övning2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lexicon_övning2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
