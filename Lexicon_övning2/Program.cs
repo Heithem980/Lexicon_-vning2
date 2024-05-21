8a4f97b8bbff3bd2a2f18bd23b7cf393413eb4dc
Heithem980 <heithem.gha98@gmail.com>
2024-05-21 16:50:35 +0200

    grejer händer :)

--- Lexicon_övning2/Program.cs
+++ Lexicon_övning2/Program.cs
@@ -1,10 +1,52 @@
-﻿namespace Lexicon_övning2
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Lexicon_övning2
 {
     internal class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+
+            string input;
+            bool run = true;
+
+            do
+            {
+                Console.WriteLine("Du har kommit till huvudmenyn. Du kan navigera genom att skriva in siffror" +
+                "för att testa olika funktioner.    ");
+                input = Console.ReadLine()!;
+
+                switch (input)
+                {
+                    case "0":
+                        Console.WriteLine("Programmet stängs ner...");
+                        run = false;
+                        break;
+                    case "1":
+                        Console.WriteLine("");
+                        run = false;
+                        break;
+                    case "2":
+                        Console.WriteLine("");
+                        run = false;
+                        break;
+                    case "3":
+                        Console.WriteLine("");
+                        run = false;
+                        break;
+                    default:
+                        Console.WriteLine("Felaktigt input, försök igen.");
+                        
+                        break;
+                }
+
+            } while (run);
+
+
         }
     }
 }
